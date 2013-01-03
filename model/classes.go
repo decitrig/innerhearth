e435f9ed89099d8209bf7214f26d82ef91db343b
Ryan W Sims <rwsims@gmail.com>
2013-01-03 18:05:57 -0500

    Migrated registration flow to new model types.

--- model/classes.go
+++ model/classes.go
@@ -10,8 +10,10 @@ import (
 )
 
 var (
-	ErrClassExists   = errors.New("Class already exists")
-	ErrClassNotEmpty = errors.New("Class is not empty")
+	ErrClassFull         = errors.New("Class is full")
+	ErrClassExists       = errors.New("Class already exists")
+	ErrClassNotEmpty     = errors.New("Class is not empty")
+	ErrAlreadyRegistered = errors.New("Student is already registered for class")
 )
 
 type ClassFullError struct {
@@ -27,6 +29,7 @@ func classFullError(class string) error {
 }
 
 type Class struct {
+	ID          int64  `datastore: "-"`
 	Title       string `datastore: ",noindex"`
 	Description string `datastore: ",noindex"`
 	Teacher     string
@@ -37,6 +40,7 @@ type Class struct {
 
 	BeginDate time.Time
 	EndDate   time.Time
+	Active    bool
 
 	DropInOnly bool
 
@@ -51,49 +55,59 @@ func (c *Class) NextClassTime(after time.Time) time.Time {
 }
 
 func (c *Class) key(ctx appengine.Context) *datastore.Key {
-	return datastore.NewKey(ctx, "Class", c.Title, 0, nil)
+	return datastore.NewKey(ctx, "Class", "", c.ID, nil)
 }
 
 // A Scheduler is responsible for manipulating classes.
 type Scheduler interface {
 	AddNew(c *Class) error
+	GetClass(id int64) *Class
 	ListClasses(activeOnly bool) []*Class
-	DeleteClass(class string) error
+	DeleteClass(c *Class) error
 }
 
 type scheduler struct {
 	appengine.Context
 }
 
-func (s *scheduler) AddNew(c *Class) error {
-	key := c.key(s)
-	err := datastore.RunInTransaction(s, func(context appengine.Context) error {
-		old := &Class{}
-		if err := datastore.Get(context, key, old); err != datastore.ErrNoSuchEntity {
-			return ErrClassExists
-		}
-		_, err := datastore.Put(context, key, c)
-		return err
-	}, nil)
-	if err != nil && err != ErrClassExists {
-		return fmt.Errorf("Erorr inserting class %s: %s", c.Title, err)
+func (s *scheduler) GetClass(id int64) *Class {
+	key := datastore.NewKey(s, "Class", "", id, nil)
+	class := &Class{}
+	if err := datastore.Get(s, key, class); err != nil {
+		s.Errorf("Error getting class %d: %s", id, err)
+		return nil
 	}
-	return err
+	class.ID = id
+	return class
+}
+
+func (s *scheduler) AddNew(c *Class) error {
+	key := datastore.NewIncompleteKey(s, "Class", nil)
+	if _, err := datastore.Put(s, key, c); err != nil {
+		return fmt.Errorf("Error writing class %s: %s", c.Title, err)
+	}
+	return nil
 }
 
 func (s *scheduler) ListClasses(activeOnly bool) []*Class {
 	classes := []*Class{}
 	q := datastore.NewQuery("Class")
-	_, err := q.GetAll(s, &classes)
+	if activeOnly {
+		q = q.Filter("Active =", true)
+	}
+	keys, err := q.GetAll(s, &classes)
 	if err != nil {
 		s.Errorf("Error listing classes: %s", err)
 		return nil
 	}
+	for idx, key := range keys {
+		classes[idx].ID = key.IntID()
+	}
 	return classes
 }
 
-func (s *scheduler) DeleteClass(class string) error {
-	key := datastore.NewKey(s, "Class", class, 0, nil)
+func (s *scheduler) DeleteClass(c *Class) error {
+	key := c.key(s)
 	err := datastore.RunInTransaction(s, func(context appengine.Context) error {
 		q := datastore.NewQuery("Registration").
 			Ancestor(key).
@@ -112,7 +126,7 @@ func (s *scheduler) DeleteClass(class string) error {
 		return nil
 	}, nil)
 	if err != nil && err != ErrClassNotEmpty {
-		return fmt.Errorf("Error deleting %s: %s", class, err)
+		return fmt.Errorf("Error deleting class %d: %s", c.ID, err)
 	}
 	return err
 }
@@ -124,28 +138,103 @@ func NewScheduler(c appengine.Context) Scheduler {
 // A Registration represents a reserved space in a class, either for the entire session or as a drop
 // in.
 type Registration struct {
-	StudentID  string
-	ClassTitle string
+	StudentID string
+	ClassID   int64
 
 	// If this registration is a drop in, Date is the start time of the class to which it applies.
 	Date time.Time
 }
 
-// A Registrar manages how students register for classes in the schedule.
-type Registrar interface {
-	LookupRegistration(class, studentID string) *Registration
+func (r *Registration) key(c appengine.Context) *datastore.Key {
+	classKey := datastore.NewKey(c, "Class", "", r.ClassID, nil)
+	return datastore.NewKey(c, "Registration", r.StudentID, 0, classKey)
 }
 
-func NewRegistrar(c appengine.Context) Registrar {
-	return nil
-}
-
-type StudentRegistrar interface {
+type Roster interface {
+	LookupRegistration(studentID string) *Registration
 	ListRegistrations() []*Registration
-	FilterRegisteredClasses(classes []*Class) ([]*Class, error)
-	RegisterForClass(c *Class) (*Registration, error)
+	AddStudent(studentID string) (*Registration, error)
+	AddDropIn(studentID string, date time.Time) (*Registration, error)
 }
 
-func NewStudentRegistrar(c appengine.Context, studentID string) StudentRegistrar {
-	return nil
+type roster struct {
+	appengine.Context
+	class *Class
+}
+
+func NewRoster(c appengine.Context, class *Class) Roster {
+	return &roster{c, class}
+}
+
+func (r *roster) ListRegistrations() []*Registration {
+	q := datastore.NewQuery("Registration").
+		Ancestor(r.class.key(r))
+	rs := []*Registration{}
+	if _, err := q.GetAll(r, rs); err != nil {
+		r.Errorf("Error looking up registrations for class %d: %s", r.class.ID, err)
+		return nil
+	}
+	return rs
+}
+
+func (r *roster) LookupRegistration(studentID string) *Registration {
+	key := datastore.NewKey(r, "Registration", studentID, 0, r.class.key(r))
+	reg := &Registration{}
+	if err := datastore.Get(r, key, reg); err != nil {
+		r.Errorf("Error looking up registration for student %s in class %d: %s", studentID, r.class.ID, err)
+		return nil
+	}
+	return reg
+}
+
+func (r *roster) AddStudent(studentID string) (*Registration, error) {
+	reg := &Registration{
+		ClassID:   r.class.ID,
+		StudentID: studentID,
+	}
+	err := datastore.RunInTransaction(r, func(ctx appengine.Context) error {
+		key := reg.key(ctx)
+		old := &Registration{}
+		if err := datastore.Get(ctx, key, old); err != datastore.ErrNoSuchEntity {
+			if err != nil {
+				return fmt.Errorf("Error looking up registration %+v: %s", reg, err)
+			}
+			return ErrAlreadyRegistered
+		}
+		if _, err := datastore.Put(ctx, key, reg); err != nil {
+			return fmt.Errorf("Error writing registration %+v: %s", reg, err)
+		}
+		return nil
+	}, nil)
+	if err != nil {
+		return nil, err
+	}
+	return reg, nil
+}
+
+func (r *roster) AddDropIn(studentID string, date time.Time) (*Registration, error) {
+	return nil, fmt.Errorf("Not yet implemented")
+}
+
+type Registrar interface {
+	ListRegistrations() []*Registration
+}
+
+type registrar struct {
+	appengine.Context
+	studentID string
+}
+
+func NewRegistrar(c appengine.Context, studentID string) Registrar {
+	return &registrar{c, studentID}
+}
+
+func (r *registrar) ListRegistrations() []*Registration {
+	rs := []*Registration{}
+	q := datastore.NewQuery("Registration").
+		Filter("StudentID =", r.studentID)
+	if _, err := q.GetAll(r, &rs); err != nil {
+		return nil
+	}
+	return rs
 }

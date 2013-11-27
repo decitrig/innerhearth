ce1ca38ae274160444317a97dc270603b9748532
Ryan W Sims <rwsims@gmail.com>
2013-11-27 15:43:09 -0500

    Implemented release script.

---
+++ push.sh
@@ -0,0 +1,17 @@
+prod_id=innerhearthyoga
+dev_id=innerhearth-testing
+appcfg=${HOME}/tools/go_appengine/appcfg.py
+
+appid=$dev_id
+while getopts "p" opt; do
+    case ${opt} in
+	p)
+	    appid=$prod_id
+	    ;;
+    esac
+done
+
+$appcfg update \
+    innerhearth \
+    --oauth2 \
+    -A $appid

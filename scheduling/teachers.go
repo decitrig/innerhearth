2923d172ef562d97339cd81e3cee715f9ff55bda
Ryan W Sims <rwsims@gmail.com>
2013-12-22 18:33:56 -0500

    Added teachers.

---
+++ scheduling/teachers.go
@@ -0,0 +1,93 @@
+package scheduling
+
+import (
+	"appengine"
+	"appengine/datastore"
+
+	"github.com/decitrig/innerhearth/auth"
+)
+
+// A Teacher can be assigned to one or more classes or workshops, and
+// is allowed to view rosters for classes & workshops they teach.
+type Teacher struct {
+	// The InnerHearthUser account with which this teacher is associated.
+	AccountID string `datastore: "-"`
+
+	// Contact information for the teacher. This is identical to the
+	// information in the teachers' InnerHearthUser account.
+	auth.UserInfo
+}
+
+// Creates a new Teacher associated with the given user.
+func NewTeacher(user *auth.InnerHearthUser) *Teacher {
+	return &Teacher{
+		AccountID: user.AccountID,
+		UserInfo:  user.UserInfo,
+	}
+}
+
+// ByName sorts teachers in alphabetical order by first and then last name.
+type ByName []*Teacher
+
+func (l ByName) Len() int      { return len(l) }
+func (l ByName) Swap(i, j int) { l[i], l[j] = l[j], l[i] }
+func (l ByName) Less(i, j int) bool {
+	if l[i].FirstName != l[j].FirstName {
+		return l[i].FirstName < l[j].FirstName
+	}
+	return l[i].LastName < l[j].LastName
+}
+
+func teacherKeyFromID(c appengine.Context, id string) *datastore.Key {
+	return datastore.NewKey(c, "Teacher", id, 0, nil)
+}
+
+func (t *Teacher) key(c appengine.Context) *datastore.Key {
+	return teacherKeyFromID(c, t.AccountID)
+}
+
+func lookupTeacher(c appengine.Context, key *datastore.Key) (*Teacher, error) {
+	teacher := &Teacher{}
+	if err := datastore.Get(c, key, teacher); err != nil {
+		if err != datastore.ErrNoSuchEntity {
+			c.Errorf("Error looking up teacher %q: %s", key.StringID(), err)
+		}
+		return nil, auth.ErrUserNotFound
+	}
+	return teacher, nil
+}
+
+// LookupTeacher returns the Teacher associated with the given user, if one exists.
+func LookupTeacher(c appengine.Context, user *auth.InnerHearthUser) (*Teacher, error) {
+	return lookupTeacher(c, teacherKeyFromID(c, user.AccountID))
+}
+
+// LookupTeacher returns the Teacher with thte given ID, if one exists.
+func LookupTeacherByID(c appengine.Context, id string) (*Teacher, error) {
+	return lookupTeacher(c, teacherKeyFromID(c, id))
+}
+
+// Store persists the Teacher to the datastore.
+func (t *Teacher) Store(c appengine.Context) error {
+	key := t.key(c)
+	if _, err := datastore.Put(c, key, t); err != nil {
+		return err
+	}
+	return nil
+}
+
+// AllTeachers returns a list of all the Teachers which currently exist.
+func AllTeachers(c appengine.Context) []*Teacher {
+	q := datastore.NewQuery("Teacher").
+		Limit(100)
+	teachers := []*Teacher{}
+	keys, err := q.GetAll(c, &teachers)
+	if err != nil {
+		c.Errorf("Failed to look up teachers: %s", err)
+		return nil
+	}
+	for i, key := range keys {
+		teachers[i].AccountID = key.StringID()
+	}
+	return teachers
+}
---
+++ scheduling/teachers_test.go
@@ -0,0 +1,95 @@
+package scheduling
+
+import (
+	"reflect"
+	"sort"
+	"testing"
+
+	"appengine/aetest"
+
+	"github.com/decitrig/innerhearth/auth"
+)
+
+func TestNewTeacher(t *testing.T) {
+	ihu := &auth.InnerHearthUser{
+		AccountID: "0xdeadbeef",
+		UserInfo: auth.UserInfo{
+			FirstName: "First",
+			LastName:  "last",
+			Email:     "foo@bar.com",
+		},
+	}
+	expected := &Teacher{
+		AccountID: ihu.AccountID,
+		UserInfo:  ihu.UserInfo,
+	}
+	if teacher := NewTeacher(ihu); !reflect.DeepEqual(expected, teacher) {
+		t.Errorf("Wrong teacher created; %v vs %v", teacher, expected)
+	}
+}
+
+func usersToTeachers(users []*auth.InnerHearthUser) []*Teacher {
+	teachers := make([]*Teacher, len(users))
+	for i, user := range users {
+		teachers[i] = NewTeacher(user)
+	}
+	return teachers
+}
+
+func TestTeacher(t *testing.T) {
+	users := []*auth.InnerHearthUser{{
+		AccountID: "0x1",
+		UserInfo: auth.UserInfo{
+			FirstName: "a",
+			LastName:  "b",
+			Email:     "a@example.com",
+		}}, {
+		AccountID: "0x2",
+		UserInfo: auth.UserInfo{
+			FirstName: "aa",
+			LastName:  "bb",
+			Email:     "aa@example.com",
+		}}, {
+		AccountID: "0x3",
+		UserInfo: auth.UserInfo{
+			FirstName: "aaa",
+			LastName:  "bbb",
+			Email:     "aaa@example.com",
+		}}}
+	c, err := aetest.NewContext(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer c.Close()
+	for i, user := range users {
+		if _, err := LookupTeacher(c, user); err == nil {
+			t.Errorf("Shouldn't have found teacher for user %d", i)
+		}
+		teacher := NewTeacher(user)
+		if err := teacher.Store(c); err != nil {
+			t.Fatalf("Failed to store teacher %d: %s", i, err)
+		}
+		if found, err := LookupTeacher(c, user); err != nil {
+			t.Errorf("Didn't find teacher %d: %s", i, err)
+		} else if !reflect.DeepEqual(teacher, found) {
+			t.Errorf("Found wrong teacher; %v vs %v", found, teacher)
+		}
+		if found, err := LookupTeacherByID(c, user.AccountID); err != nil {
+			t.Errorf("Didn't find techer %d: %s", i, err)
+		} else if !reflect.DeepEqual(teacher, found) {
+			t.Errorf("Found wrong teacher by id; %v vs %v", found, teacher)
+		}
+	}
+	expected := usersToTeachers(users)
+	allTeachers := AllTeachers(c)
+	if len(allTeachers) != len(expected) {
+		t.Fatalf("Wrong number of teachers returned; %d vs %d", len(allTeachers), len(expected))
+	}
+	sort.Sort(ByName(expected))
+	sort.Sort(ByName(allTeachers))
+	for i, want := range expected {
+		if got := allTeachers[i]; !reflect.DeepEqual(got, want) {
+			t.Errorf("Wrong teacher at %d; %v vs %v", i, got, want)
+		}
+	}
+}
